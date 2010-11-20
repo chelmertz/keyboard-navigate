0fc9e28744245a248bd53c4a6b7a27b1fbe25052
Carl Helmertz <helmertz@gmail.com>
2010-11-21 00:49:53 +0100

    Add: more flexibility, extend the plugin with a keyMap or callback of your own; the old functionality is now default

--- jquery.keyboardNavigate.js
+++ jquery.keyboardNavigate.js
@@ -5,27 +5,36 @@ jQuery.fn.keyboardNavigate = function(options) {
 		return this;
 	}
 
-	var keyMap = {
-		39: 'next', // right
-		37: 'previous', // left
-		38: 'up', // up
-		40: 'down', // down
-		80: 'previous', // p
-		27: 'up', // esc
-		78: 'next', // n
-		74: 'next', // j
-		75: 'previous' // k
+	var settings = {
+		callback: function(keyPressed, keyMap, event) {
+			if(!$.inArray(keyPressed, keyMap)) {
+				return;
+			}
+			var affectedElement = navigationContainer.find('a.'+keyMap[keyPressed]);
+			if(affectedElement.length) {
+				window.location.href = affectedElement.attr('href');
+			}
+		},
+		keyMap: {
+			39: 'next', // right
+			37: 'previous', // left
+			38: 'up', // up
+			40: 'down', // down
+			80: 'previous', // p
+			27: 'up', // esc
+			78: 'next', // n
+			74: 'next', // j
+			75: 'previous' // k
+		}
 	};
+
+	if(options) {
+		$.extend(settings, options);
+	}
 
 	$('body').keydown(function(event) {
 		var keyPressed = event.which;
-		if(!$.inArray(keyPressed, keyMap)) {
-			return;
-		}
-		var affectedElement = navigationContainer.find('a.'+keyMap[keyPressed]);
-		if(affectedElement.length) {
-			window.location.href = affectedElement.attr('href');
-		}
+		settings.callback(keyPressed, settings.keyMap, event);
 	});
 
 	return this;
